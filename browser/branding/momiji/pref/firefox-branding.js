0701fdae95de67f8cea4de86ed1c8f64530224d7
aobaharuki2005 <tranbaohnth@outlook.com>
2026-03-19 09:21:49 +0700

    Update: cleanup proprietary vendors and brands, switch to Momiji

--- browser/branding/momiji/pref/firefox-branding.js
+++ browser/branding/momiji/pref/firefox-branding.js
@@ -4,8 +4,8 @@
 
 // This file contains branding-specific prefs.
 
-pref("startup.homepage_override_url", "https://www.mozilla.org/projects/firefox/%VERSION%/whatsnew/?oldversion=%OLD_VERSION%");
-pref("startup.homepage_welcome_url", "https://www.mozilla.org/projects/firefox/%VERSION%/firstrun/");
+pref("startup.homepage_override_url", "https://github.com/aobaharuki2005/momiji-web-browser");
+pref("startup.homepage_welcome_url", "https://github.com/aobaharuki2005/momiji-web-browser");
 pref("startup.homepage_welcome_url.additional", "");
 // The time interval between checks for a new version (in seconds)
 pref("app.update.interval", 7200); // 2 hours
@@ -13,14 +13,14 @@ pref("app.update.interval", 7200); // 2 hours
 pref("app.update.promptWaitTime", 43200);
 // URL user can browse to manually if for some reason all update installation
 // attempts fail.
-pref("app.update.url.manual", "https://www.mozilla.org/%LOCALE%/firefox/nightly/?reason=manual-update");
+pref("app.update.url.manual", "https://github.com/aobaharuki2005/momiji-web-browser");
 // A default value for the "More information about this update" link
 // supplied in the "An update is available" page of the update wizard.
-pref("app.update.url.details", "https://www.mozilla.org/%LOCALE%/firefox/nightly/notes/");
+pref("app.update.url.details", "https://github.com/aobaharuki2005/momiji-web-browser");
 
-pref("app.releaseNotesURL", "https://www.mozilla.org/%LOCALE%/firefox/%VERSION%/releasenotes/?utm_source=firefox-browser&utm_medium=firefox-desktop&utm_campaign=whatsnew");
-pref("app.releaseNotesURL.aboutDialog", "https://www.mozilla.org/%LOCALE%/firefox/%VERSION%/releasenotes/?utm_source=firefox-browser&utm_medium=firefox-desktop&utm_campaign=about-dialog");
-pref("app.releaseNotesURL.prompt", "https://www.mozilla.org/%LOCALE%/firefox/%VERSION%/releasenotes/?utm_source=firefox-browser&utm_medium=firefox-desktop&utm_campaign=updateprompt");
+pref("app.releaseNotesURL", "https://github.com/aobaharuki2005/momiji-web-browser");
+pref("app.releaseNotesURL.aboutDialog", "https://github.com/aobaharuki2005/momiji-web-browser");
+pref("app.releaseNotesURL.prompt", "https://github.com/aobaharuki2005/momiji-web-browser");
 
 // The number of days a binary is permitted to be old
 // without checking for an update.  This assumes that
