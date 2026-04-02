7ca6f81b380a433ec45b19b8139fe95d6e144c6b
aobaharuki2005 <tranbaohnth@outlook.com.vn>
2026-04-02 22:54:04 +0700

    Experimental: Momiji 140.3.0esr-mj + SDK 15.4 + Rust 1.82.0-nightly
    Isolate workflows and build configs for 10.7-10.8 and 10.9-10.14 target
    Use lld with wasm enabled for 10.9-10.14

---
+++ mozconfig-cicd_1009-1014.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+# ======== BUILD DESTINATION =============
+mk_add_options MOZ_OBJDIR=@TOPSRCDIR@/obj-ff-dbg-esr
+
+# ======== TARGET APPLICATION ============
+ac_add_options --enable-application=browser
+
+# ======== TARGET PLATFORM ===============
+ac_add_options --target=x86_64-apple-darwin
+export MACOSX_DEPLOYMENT_TARGET=10.9
+
+# ============= SCCACHE ==============
+ac_add_options --with-ccache="$HOME/.mozbuild/sccache/sccache"
+export SCCACHE_IDLE_TIMEOUT=0
+
+# ============= NASM & DUMPSYMS =============
+export NASM="$HOME/.mozbuild/nasm/nasm"
+export DUMP_SYMS="$HOME/.mozbuild/dump_syms/dump_syms"
+
+# ============ SDK ===================
+# Use SDK 15.4 (according to the minimum version specified in python\mozboot\mozboot\util.py
+ac_add_options --with-macos-sdk="/Applications/Xcode_16.3.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX15.4.sdk"
+
+# ============= LINKER ===============
+ac_add_options --enable-linker=lld # experimental: use lld
+
+# ============= DEBUG FLAGS ================
+export MOZ_DEBUG_FLAGS="-fdebug-default-version=2 -gdwarf-2 -gfull"
+
+# ============= NODEJS =================
+export NODEJS="$HOME/.mozbuild/node/bin/node"
+
+# ===== BRANDING =======
+ac_add_options --with-app-name=momiji
+ac_add_options --with-branding=browser/branding/momiji
+ac_add_options --with-distribution-id=net.momiji
+
+# ========== RUST ==========
+export RUST_BIN_PATH="$HOME/.rustup/toolchains/nightly-2024-07-25-x86_64-apple-darwin/bin"
+export RUSTC="$RUST_BIN_PATH/rustc"
+export CARGO="$RUST_BIN_PATH/cargo"
+export CBINDGEN="$HOME/.mozbuild/cbindgen/cbindgen"
+export RUSTFLAGS="-C link-arg=-mmacosx-version-min=10.9 -C link-arg=-headerpad_max_install_names"
+
+# ========== C/C++ ==========
+export CC="$HOME/.mozbuild/clang/bin/clang"
+export CXX="$HOME/.mozbuild/clang/bin/clang++"
+export LDFLAGS="-mmacosx-version-min=10.9 -headerpad_max_install_names"
+export CFLAGS="-mmacosx-version-min=10.9 -D__MAC_OS_X_VERSION_MIN_REQUIRED=1090 -Wl,-headerpad_max_install_names"
+export CXXFLAGS="-mmacosx-version-min=10.9 -D__MAC_OS_X_VERSION_MIN_REQUIRED=1090 -Wl,-headerpad_max_install_names"
+
+# ========== OPTIMIZATIONS ==========
+ac_add_options --disable-crashreporter
+# ac_add_options --without-wasm-sandboxed-libraries
+ac_add_options --disable-tests
+ac_add_options --disable-debug
+ac_add_options --disable-dmd
+ac_add_options --disable-geckodriver
+ac_add_options --disable-profiling
+ac_add_options --disable-updater
+
+# From Waterfox (Production build)
+# export MOZ_LTO="thin"
+# ac_add_options --enable-optimize="-march=core2 -O3 -w"
+# ac_add_options --enable-release
+# ac_add_options --enable-rust-simd
+# ac_add_options RUSTC_OPT_LEVEL=3
+# export RUSTFLAGS="$RUSTFLAGS -Ctarget-cpu=core2"
+
+# From Waterfox (development build)
+ac_add_options --enable-optimize="-Os -w"
