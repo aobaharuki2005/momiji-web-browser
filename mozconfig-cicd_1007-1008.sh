e878117881325eb0898af0975324971f99c53130
aobaharuki2005 <tranbaohnth@outlook.com>
2026-04-23 14:46:08 +0700

    CI/CD: enforce `-O2` optimization level 140.10.0esr-mj production build target 10.7-10.8 attempt#4

--- mozconfig-cicd_1007-1008.sh
+++ mozconfig-cicd_1007-1008.sh
@@ -64,12 +64,12 @@ ac_add_options --disable-sandbox # On 10.7, sandbox causes some video-streaming 
 
 # ========= Production-specific optimizations (reference from Waterfox) ===========     AMENDED! 2026-04-20
 # Remove unnecessary aggressive --enable-release and --enable-rust-simd, which may be unsuitable for very old processors (like MacPro1,1 and MacPro1,2)
-# Decrease optimization level of both Clang and Rust to -Os to ensure compabitibility on very old processors
+# Decrease optimization level of both Clang and Rust to -O2 to ensure compabitibility on very old processors
 # Relocates LDFLAGS to production-specific optimizations, which is sounder
 
 ac_add_options --disable-debug
-ac_add_options --enable-optimize="-Os -w"
-export RUSTC_OPT_LEVEL="s"
+ac_add_options --enable-optimize="-O2 -w"
+export RUSTC_OPT_LEVEL="2"
 export LDFLAGS="-headerpad_max_install_names"
 
 # ========= Testing-specific optimizations (reference from Waterfox) ===========    AMENDED! 2026-04-20
