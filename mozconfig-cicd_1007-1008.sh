606bf974d3827a7a1c4dd7b6c548168deff23ac4
Gia-Bao Tran <tranbaohnth@outlook.com.vn>
2026-04-24 22:42:44 +0700

    CI/CD: santinize build flags, force all optimization level to 0

--- mozconfig-cicd_1007-1008.sh
+++ mozconfig-cicd_1007-1008.sh
@@ -68,14 +68,17 @@ ac_add_options --disable-sandbox # On 10.7, sandbox causes some video-streaming 
 # Relocates LDFLAGS to production-specific optimizations, which is sounder
 # Enforce -march=x86-64 for compatible with old models like MacPro1,1 and MacPro1,2
 
-ac_add_options --disable-debug
-ac_add_options --enable-optimize="-march=x86-64 -Os -w"
-export RUSTC_OPT_LEVEL="s"
-export LDFLAGS="-headerpad_max_install_names -ld_classic"
-export RUSTFLAGS="-C target-cpu=x86-64 -C link-arg=-ld_classic"
+# ac_add_options --disable-debug
+# ac_add_options --enable-optimize="-march=x86-64 -Os -w"
+# export RUSTC_OPT_LEVEL="s"
+# export LDFLAGS="-headerpad_max_install_names -ld_classic"
+# export RUSTFLAGS="-C target-cpu=x86-64 -C link-arg=-ld_classic"
 
 # ========= Testing-specific optimizations (reference from Waterfox) ===========    AMENDED! 2026-04-20
 # Just simply --disable-optimize to enable faster build time
-# ac_add_options --disable-optimize
-# export CFLAGS="-w"
-# export CXXFLAGS="-w"
+ac_add_options --disable-optimize
+export RUST_OPT_LEVEL="0"
+export CFLAGS="-w"
+export CXXFLAGS="-w"
+export LDFLAGS="-headerpad_max_install_names -ld_classic"
+
